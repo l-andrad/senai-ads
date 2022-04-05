e586a2852caa2b52ade598d157e1278b621697c6
lucas.andrade <lucas_b_andrade@estudante.sesisenai.org.br>
2022-04-05 19:10:17 -0300

    OT12, 13 e 14 back-end

--- web/WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/java/br/com/coldigogeladeiras/rest/ProdutoRest.java
+++ web/WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/java/br/com/coldigogeladeiras/rest/ProdutoRest.java
@@ -1,14 +1,24 @@
 package br.com.coldigogeladeiras.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 import br.com.coldigogeladeiras.bd.Conexao;
 import br.com.coldigogeladeiras.jdbc.JDBCProdutoDAO;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import br.com.coldigogeladeiras.modelo.Produto;
 
@@ -40,6 +50,85 @@ public class ProdutoRest extends UtilRest {
 			return this.buildResponse(msg);
 			
 		} catch (Exception e) {
+			e.printStackTrace();
+			return this.buildErrorResponse(e.getMessage());
+		}
+	}
+	
+	@GET
+	@Path("/buscar")
+	@Consumes("application/*")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response buscarPorNome(@QueryParam("valorBusca") String nome) {
+		
+		try {
+			
+			List<JsonObject> listaProdutos = new ArrayList<JsonObject>();
+			
+			Conexao conec = new Conexao();
+			Connection conexao = conec.abrirConexao();
+			JDBCProdutoDAO jdbcProduto = new JDBCProdutoDAO(conexao);
+			listaProdutos = jdbcProduto.buscarPorNome(nome);
+			conec.fecharConexao();
+			
+			String json = new Gson().toJson(listaProdutos);
+			return this.buildResponse(json);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return this.buildErrorResponse(e.getMessage());
+		}
+		
+	}
+	
+	@DELETE
+	@Path("/excluir/{id}")
+	@Consumes("application/*")
+	public Response excluir(@PathParam("id") int id) {
+		
+		try {
+			Conexao conec = new Conexao();
+			Connection conexao = conec.abrirConexao();
+			JDBCProdutoDAO jdbcProduto = new JDBCProdutoDAO(conexao);
+			
+			boolean retorno = jdbcProduto.deletar(id);
+			
+			String msg = "";
+			if(retorno) {
+				msg = "Produto excluído com sucesso!";
+			} else {
+				msg = "Erro ao excluir produto.";
+			}
+			
+			conec.fecharConexao();
+			
+			return this.buildResponse(msg);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			return this.buildErrorResponse(e.getMessage());
+		}
+		
+	}
+	
+	@GET
+	@Path("/buscarPorId")
+	@Consumes("application/*")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response buscarPorId(@QueryParam("id") int id) {
+		
+		try {
+			Produto produto = new Produto();
+			Conexao conec = new Conexao();
+			Connection conexao = conec.abrirConexao();
+			JDBCProdutoDAO jdbcProduto = new JDBCProdutoDAO(conexao);
+			
+			produto = jdbcProduto.buscarPorId(id);
+			
+			conec.fecharConexao();
+			
+			return this.buildResponse(produto);
+		}catch(Exception e) {
 			e.printStackTrace();
 			return this.buildErrorResponse(e.getMessage());
 		}
--- web/WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/webapp/js/product.js
+++ web/WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/webapp/js/product.js
@@ -3,7 +3,12 @@ COLDIGO.produto = new Object();
 $(document).ready(function() {
 
 	//Carrega as marcas registradas no BD no select do formulário de inserir
-	COLDIGO.produto.carregarMarcas = function() {
+	COLDIGO.produto.carregarMarcas = function(id) {
+		if(id!=undefined){
+			select = "#selMarcaEdicao";
+		}else{
+			select = "#selMarca";
+		}
 
 		$.ajax({
 			type: "GET",
@@ -12,28 +17,32 @@ $(document).ready(function() {
 
 				if (marcas != "") {
 
-					$("#selMarca").html("");
+					$(select).html("");
 					var option = document.createElement("option");
 					option.setAttribute("value", "");
 					option.innerHTML = ("Escolha");
-					$("#selMarca").append(option);
+					$(select).append(option);
 
 					for (var i = 0; i < marcas.length; i++) {
 						var option = document.createElement("option");
 						option.setAttribute("value", marcas[i].id);
+						
+						if((id!=undefined)&&(id==marcas[i].id))
+							option.setAttribute("selected", "selected");
+							
 						option.innerHTML = (marcas[i].nome);
-						$("#selMarca").append(option);
+						$(select).append(option);
 					}
 
 				} else {
 
-					$("#selMarca").html("");
+					$(select).html("");
 
 					var option = document.createElement("option");
 					option.setAttribute("value", "");
 					option.innerHTML = ("Cadastre uma marca primeiro!");
-					$("#selMarca").append(option);
-					$("#selMarca").addClass("aviso");
+					$(select).append(option);
+					$(select).addClass("aviso");
 				}
 
 			},
@@ -41,12 +50,12 @@ $(document).ready(function() {
 
 				COLDIGO.exibirAviso("Erro ao buscar as marcas: " + info.status + " - " + info.statusText);
 
-				$("#selMarca").html("");
+				$(select).html("");
 				var option = document.createElement("option");
 				option.setAttribute("value", "");
 				option.innerHTML = ("Erro ao carregar marcas!");
-				$("#selMarca").append(option);
-				$("#selMarca").addClass("aviso");
+				$(select).append(option);
+				$(select).addClass("aviso");
 
 			}
 		});
@@ -86,6 +95,134 @@ $(document).ready(function() {
 		}
 	}
 	
+	//Busca no BD e exibe na página os produtos que atendam à solicitação do usuário
+	COLDIGO.produto.buscar = function() {
+		
+		var valorBusca = $("#campoBuscaProduto").val();
+		
+		$.ajax({
+			type: "GET",
+			url: COLDIGO.PATH + "produto/buscar",
+			data: "valorBusca="+valorBusca,
+			success: function(dados) {
+				
+				dados = JSON.parse(dados);
+				$("#listaProdutos").html(COLDIGO.produto.exibir(dados));
+				
+				
+			},
+			error: function(info) {
+				COLDIGO.exibirAviso("Erro ao consultar os produtos: " + info.status + " - " + info.statusText);
+			}
+		});
+		
+	};
 	
+	//Transforma os dados dos produtos recebidos do servidor em uma tabela HTML
+	COLDIGO.produto.exibir = function(listaDeProdutos) {
+		
+		var tabela = "<table>" +
+		"<tr>" +
+		"<th>Categoria</th>" +
+		"<th>Marca</th>" +
+		"<th>Modelo</th>" +
+		"<th>Cap. (l)</th>" +
+		"<th>Valor</th>" +
+		"<th class='acoes'>Ações</th>" +
+		"</tr>";
+		
+		if (listaDeProdutos != undefined && listaDeProdutos.length > 0) {
+			
+			for(var i=0; i<listaDeProdutos.length; i++) {
+				tabela += "<tr>" +
+						"<td>" + listaDeProdutos[i].categoria + "</td>" +
+						"<td>" + listaDeProdutos[i].marcaNome + "</td>" +
+						"<td>" + listaDeProdutos[i].modelo + "</td>" +
+						"<td>" + listaDeProdutos[i].capacidade + "</td>" +
+						"<td>R$ " + COLDIGO.formatarDinheiro(listaDeProdutos[i].valor) + "</td>" +
+						"<td>" +
+							"<a onclick=\"COLDIGO.produto.exibirEdicao('"+listaDeProdutos[i].id+"')\"><img src='../../imgs/edit.png' alt='Editar registro'></a>" +
+							"<a onclick=\"COLDIGO.produto.excluir('"+listaDeProdutos[i].id+"')\"><img src='../../imgs/delete.png' alt='Excluir registro'></a>" +
+						"</td>" +
+						"</tr>"
+			}
+			
+		} else if (listaDeProdutos == "") {
+			tabela += "<tr><td colspan='6'>Nenhum registro encontrado</td></tr>";
+		}
+		tabela += "</table>";
+		
+		return tabela;
+		
+	}
+	
+	//Executa a função de busca ao carregar a página
+	COLDIGO.produto.buscar();
+	
+	//Exclui o produto selecionado
+	COLDIGO.produto.excluir = function(id) {
+		$.ajax({
+			type: "DELETE",
+			url: COLDIGO.PATH + "produto/excluir/"+id,
+			success: function(msg) {
+				COLDIGO.exibirAviso(msg);
+				COLDIGO.produto.buscar();
+			},
+			error: function(info) {
+				COLDIGO.exibirAviso("Erro ao excluir produto: "+info.status+" - " + info.statusText);
+			}
+		});
+	};
+	
+	//Carrega no BD os dados do produto selecionado para alteração e coloca-os no formulário de alteração
+	COLDIGO.produto.exibirEdicao = function(id) {
+		$.ajax({
+			type: "GET",
+			url: COLDIGO.PATH + "produto/buscarPorId",
+			data: "id="+id,
+			success: function(produto) {
+				
+				document.frmEditaProduto.idProduto.value = produto.id;
+				document.frmEditaProduto.modelo.value = produto.modelo;
+				document.frmEditaProduto.capacidade.value = produto.capacidade;
+				document.frmEditaProduto.valor.value = produto.valor;
+				
+				var selCategoria = document.getElementById('selCategoriaEdicao');
+				for(var i=0; i < selCategoria.length; i++){
+					if(selCategoria.options[i].value == produto.categoria) {
+						selCategoria.options[i].setAttribute("selected", "selected");
+					}else{
+						selCategoria.options[i].removeAttribute("selected");
+					}
+				}
+				
+				COLDIGO.produto.carregarMarcas(produto.marcaId);
+				
+				var modalEditaProduto = {
+					title: "Editar Produto",
+					height: 400,
+					width: 550,
+					modal: true,
+					buttons: {
+						"Salvar": function() {
+							
+						},
+						"Cancelar": function(){
+							$(this).dialog("close");
+						}
+					},
+					close: function() {
+						//caso o usuário simplesmente feche a caixa de edição
+						//não deve acontecer nada
+					}
+				};
+				
+				$("#modalEditaProduto").dialog(modalEditaProduto);
+			},
+			error: function(info) {
+				COLDIGO.exibirAviso("Erro ao buscar produto para edição: "+info.status+ " - " + info.statusText);
+			}
+		});
+	};
 
 });
